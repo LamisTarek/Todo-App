b629c9de981c1e307a68d30153ff2c94cf45641a
LamisTarek9 <ltarek@noon.com>
2025-08-27 16:15:43 +0300

    fix deploy for vercel

--- src/app/todos/[id]/edit/page.tsx
+++ src/app/todos/[id]/edit/page.tsx
@@ -5,8 +5,9 @@ export default async function EditTodoPage({
 }: {
     params: { id: string };
 }) {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${params.id}`,
+    const { id } = await params;
+    const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${id}`,
     { cache: "no-store" }
   );
   const todo = await res.json();
